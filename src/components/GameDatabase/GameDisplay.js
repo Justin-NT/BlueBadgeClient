d4543668cf93c3576e43bb564437295c61abee1b
Berran Micheals <me@gmail.com>
2020-01-26 21:19:47 -0500

    added cards for display

--- src/components/GameDatabase/GameDisplay.js
+++ src/components/GameDatabase/GameDisplay.js
@@ -1,10 +1,56 @@
 import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import Card from "@material-ui/core/Card";
+import CardActionArea from "@material-ui/core/CardActionArea";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+import CardMedia from "@material-ui/core/CardMedia";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+const img = '../../assets/200px-A_dragon.png"';
+
+const useStyles = makeStyles({
+  card: {
+    maxWidth: 345
+  },
+  media: {
+    height: 140
+  }
+});
 
 const GameDisplay = props => {
+  const classes = useStyles();
+
   return (
-    <div>
-      <h6>{props.game.name}</h6>
-    </div>
+    <Card className={classes.card}>
+      <CardActionArea>
+        <CardMedia
+          className={classes.media}
+          image={
+            props.game.background_image !== null
+              ? props.game.background_image
+              : img
+          }
+          title="Game Image"
+        />
+        <CardContent>
+          <Typography gutterBottom variant="h5" component="h2">
+            {props.game.name}
+          </Typography>
+          <Typography variant="body2" color="textSecondary" component="p">
+            {props.game.rating}
+          </Typography>
+        </CardContent>
+      </CardActionArea>
+      <CardActions>
+        <Button size="small" color="primary">
+          Add to Game Bar
+        </Button>
+        <Button size="small" color="primary">
+          Learn More
+        </Button>
+      </CardActions>
+    </Card>
   );
 };
 
